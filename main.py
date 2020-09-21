c389af507a7d8e20e8b04c7e3bd6cec359738143
brennerhaverlock <brenner.haverlock@gmail.com>
2020-09-21 17:25:25 -0400

    skeleton
    skeleton project

---
+++ main.py
@@ -0,0 +1,29 @@
+
+
+from random import choice
+
+# combine functions and conditionals to get a response from the bot
+
+
+def get_topic(topic_response):
+
+    # add some bot responses to this list
+    inspiration = ["""“Be yourself; everyone else is already taken.”
+                                                ― Oscar Wilde  """,
+                   """“Be the change that you wish to see in the world.”
+                                              ― Mahatma Gandhi """,
+                   """“No one can make you feel inferior without your consent.”
+                                              ― Eleanor Roosevelt""",
+                   """“I have not failed. I've just found 10,000 ways that won't work.”
+                                            ― Thomas A. Edison  """,
+                   """ “It is never too late to be what you might have been.”
+                                                ― George Eliot   """]
+    religion = ["""“Anyone who thinks sitting in church can make you a Christian must also think that sitting in a garage can make you a car.”
+― Garrison Keillor """,
+                """“The Christian does not think God will love us because we are good, but that God will make us good because He loves us.”
+― C.S. Lewis """,
+                ]
+    humor = []
+    wisdom = []
+    death = []
+    romance[]
